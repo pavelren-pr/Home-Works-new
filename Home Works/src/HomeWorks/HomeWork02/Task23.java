3ab34709cdc9174ac8c3c992a4ef05a9f787591d
pavelren-pr <pavelren.pr@gmail.com>
2025-07-16 16:10:14 +0300

    16/07/2025

---
+++ Home Works/src/HomeWorks/HomeWork02/Task23.java
@@ -0,0 +1,19 @@
+package HomeWorks.HomeWork02;
+
+import java.util.Scanner;
+
+public class Task23 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите текст: ");
+        String words = sc.nextLine();
+        System.out.println("Сколько раз вывести строку "+words+"?");
+        int count = sc.nextInt();
+        System.out.print("После повторения "+count+" раз: ");
+        for (int i = 1; i <= count; i++) {
+            System.out.print(words);
+        }
+    }
+}
+
---
+++ Home Works/src/HomeWorks/HomeWork04/Task43.java
@@ -0,0 +1,33 @@
+package HomeWorks.HomeWork04;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task43 {
+
+    // Создаём метод сортировки отдельного слова
+    public static String sortString(String tempString)
+    {
+        char tempArray[] = tempString.toCharArray();
+
+        Arrays.sort(tempArray);
+
+        return new String(tempArray);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите последовательности символов буквами латинского алфавита: ");
+        String input = sc.nextLine().toLowerCase(); // Принимаем ввод
+        String[] divided = input.split(" "); // Разделяем ввод в массив
+        System.out.println("Отсортированная строка: ");
+
+        // Сортируем каждый элемент массива при помощи метода и тут же выводим его
+        for (String tempString : divided) {
+            System.out.print(sortString(tempString)+" ");
+
+        }
+    }
+}
